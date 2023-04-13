9967d8d689188719c00fe4969fbe49e37ba5955c
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2023-04-12 22:22:44 -0300

    Calling service

--- internal/app/controller/controller.go
+++ internal/app/controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 
 	"go-observability-example/internal/app/service"
 )
@@ -25,13 +26,34 @@ func New(deps ControllerDeps) Controller {
 }
 
 func (c *controller) CreateVehicle(ctx *gin.Context) {
-
+	resp, err := c.service.CreateVehicle(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"plate": resp})
 }
 
 func (c *controller) GetVehicle(ctx *gin.Context) {
-
+	resp, err := c.service.GetVehicle(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": resp})
 }
 
 func (c *controller) GetVehicleByPlate(ctx *gin.Context) {
+	plate := ctx.Param("plate")
+	if plate == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "plate is empty"})
+		return
+	}
 
+	resp, err := c.service.GetVehicleByPlate(ctx, plate)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": resp})
 }
